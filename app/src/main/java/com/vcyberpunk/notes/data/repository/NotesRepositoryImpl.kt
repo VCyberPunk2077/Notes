f77aedce25400f874e0c21cb6fde348b9bcbf31b
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-06-24 22:44:45 +0300

    feat: add image support to notes repository

--- app/src/main/java/com/vcyberpunk/notes/data/repository/NotesRepositoryImpl.kt
+++ app/src/main/java/com/vcyberpunk/notes/data/repository/NotesRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.vcyberpunk.notes.data.repository
 
+import com.vcyberpunk.notes.data.local.ImageFileManager
 import com.vcyberpunk.notes.data.local.db.NotesDao
 import com.vcyberpunk.notes.data.mapper.toDbModel
 import com.vcyberpunk.notes.data.mapper.toEntity
@@ -12,7 +13,8 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class NotesRepositoryImpl @Inject constructor(
-    private val notesDao: NotesDao
+    private val notesDao: NotesDao,
+    private val imageFileManager: ImageFileManager
 ) : NotesRepository {
 
     override suspend fun addNote(
@@ -24,7 +26,7 @@ class NotesRepositoryImpl @Inject constructor(
         val note = Note(
             id = UNDEFINED_ID,
             title = title,
-            content = content,
+            content = content.processedForStorage(),
             updatedAt = updatedAt,
             isPinned = isPinned
         )
@@ -33,11 +35,35 @@ class NotesRepositoryImpl @Inject constructor(
     }
 
     override suspend fun deleteNote(noteId: Int) {
+        val note = notesDao.getNote(noteId).toEntity()
         notesDao.deleteNote(noteId = noteId)
+
+        note.content
+            .filterIsInstance<ContentItem.Image>()
+            .map { it.url }
+            .forEach {
+                imageFileManager.deleteImage(it)
+            }
     }
 
     override suspend fun editNote(note: Note) {
-        notesDao.addNote(note.toDbModel())
+        val oldNote = notesDao.getNote(note.id).toEntity()
+        val oldUrls = oldNote.content
+            .filterIsInstance<ContentItem.Image>()
+            .map { it.url }
+
+        val newUrls = note.content
+            .filterIsInstance<ContentItem.Image>()
+            .map { it.url }
+        val removedUrls = oldUrls - newUrls.toSet()
+        removedUrls.forEach {
+            imageFileManager.deleteImage(it)
+        }
+
+        val processedContent = note.content.processedForStorage()
+        val processedNote = note.copy(content = processedContent)
+
+        notesDao.addNote(processedNote.toDbModel())
     }
 
     override fun getAllNotes(): Flow<List<Note>> = notesDao.getAllNotes().map { it.toListEntity() }
@@ -49,6 +75,26 @@ class NotesRepositoryImpl @Inject constructor(
 
     override suspend fun switchPinnedStatus(noteId: Int) {
         notesDao.switchPinnedStatus(noteId)
+    }
+
+    private suspend fun List<ContentItem>.processedForStorage(): List<ContentItem> {
+        return map { contentItem ->
+            when (contentItem) {
+                is ContentItem.Image -> {
+                    if (imageFileManager.isInternal(contentItem.url)) {
+                        contentItem
+                    } else {
+                        val internalPath =
+                            imageFileManager.copyImageToInternalStorage(contentItem.url)
+                        ContentItem.Image(
+                            url = internalPath
+                        )
+                    }
+                }
+
+                is ContentItem.Text -> contentItem
+            }
+        }
     }
 
     companion object {
